3f81e920659ef1ef00b918278be4ab656ced2ec4
Gaspar <gaspar.zanellini@gmail.com>
2016-09-17 21:45:33 -0300

    arregle un par de bugs que tenia al  comprimir el archivo

--- logep.sh
+++ logep.sh
@@ -6,11 +6,11 @@ tipo=[INFO]
 #check si LOGDIR esta fijada
 if [ -z "$LOGDIR" ]
 then
-	printf "%b\n" "la variable LOGDIR esta vacia"
+	printf "%b\n" "logep: la variable LOGDIR esta vacia">&2
 	exit
 elif [ ! -d "$LOGDIR" ]
 then
-	printf "%b\n" "la variable LOGDIR no contiene un directorio valido"
+	printf "%b\n" "logep: la variable LOGDIR no contiene un directorio valido">&2
 	exit
 fi	
 
@@ -24,7 +24,8 @@ do
 	w) tipo=[WARM];;
 	e) tipo=[ERR];;
 	h) printf "Modo de empleo: logep [opcion] [comando] [mensaje]\n"
-		 printf "Graba en el archivo de log del comando el mensaje en formato W5. De no existir el archivo lo crea\n"
+		 printf " %b\n" "Graba en el archivo de log del comando el mensaje en formato W5. De no existir el archivo lo crea" 
+		 printf "El mensaje no debe contener caracters de nueva linea, el comando tampoco\n"
 		 printf "La variable global LOGDIR debe ser fijada con el directorio del archivo de log, antes de grabar cualquier log\n"
 		 printf "Tipos:\n"
 		 printf " %b\n" "-i graba un mensaje de tipo info"  "-w graba un mensaje de tipo warm" "-e graba un mensaje de tipo error"
@@ -41,12 +42,24 @@ shift $[ $OPTIND - 1 ]
 #revisa que este los parametros obligatorios
 if [ $# -lt 2 ]
 then
-	printf "%b\n" "logep: falta alguno de los parametros obligatorios [comando] o [mensaje]"
+	printf "%b\n" "logep: falta alguno de los parametros obligatorios [comando] o [mensaje]">&2
 	exit
 fi
 
-#si el archivo no existe lo creo.
-logfile="$LOGDIR/$1.log"
+#revisa que no haya saltos de linea
+if [[ "$1" == *$'\n'* ]] || [[ $2 == *$'\n'* ]]
+then
+	printf "logep: mensaje y comando no deben tener saltos de lineas\n">&2
+	exit
+fi
+
+
+#guarda el directorio actual
+act_d=´pwd´
+cd "$LOGDIR"
+
+#archivo de log
+logfile="$1.log"
 
 printf "%b\n" "$USERNAME `date +%D` $1 $tipo $2">>"$logfile"
 
@@ -63,16 +76,17 @@ log_excedido="Log Excedido"
 
 printf "%s\n" "$log_excedido"
 
-#creo un archivo temporal de trabajo
-work_file="$LOGDIR/temp$1.log"
+#crea un archivo temporal de trabajo
+work_file="temp$1.log"
 
-#guardo las primeras max_lines/2 lineas del log en un archivo comprimido
+#guarda las primeras max_lines/2 lineas del log en un archivo comprimido
 head -n $[max_lines/2] "$logfile" >"$work_file"
 zip "$logfile.zip" "$work_file"
 
-#en el original dejo las restantes lineas
+#en el original deja las restantes lineas
 printf "%b\n" "$USERNAME `date +%D` $1 $tipo $log_excedido">"$work_file"
 tail -n $[max_lines/2] "$logfile" >>"$work_file"
 cat "$work_file">"$logfile"
 rm "$work_file"
+cd $act_d
 
